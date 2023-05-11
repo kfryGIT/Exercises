958eb8bbb7bd8a252ecc2f95c91f5f6ab530d477
Katarzyna Madalinska <kfryczka86@wp.pl>
2023-05-11 15:48:42 +0200

    add stream exercise

---
+++ Codingbat123.java
@@ -0,0 +1,27 @@
+package abc_Samouczek;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Codingbat123 {
+	
+	public static List<Integer> square(List<Integer> nums) {
+		  return nums.stream()
+		  .map(el->el*el)
+		  .collect(Collectors.toList());
+		}
+
+	public static void main(String[] args) {
+		
+		List <Integer>list1=new ArrayList<>();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		System.out.print(list1);
+		System.out.print(
+		square(list1));
+
+	}
+
+}
